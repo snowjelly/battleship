05efa1dcaf80c013b3889f308a725f6c2ac0baa3
snowjelly <broadwellconnor@gmail.com>
2024-01-24 14:22:45 -0600

    pass first test

---
+++ battleship.js
@@ -0,0 +1,5 @@
+const Ship = (length) => {
+  return { length: 2, sunk: false, hit: 0 };
+};
+
+export { Ship };
--- battleship.test.js
+++ battleship.test.js
@@ -1,8 +1,8 @@
-test(
-  "2 length ship with full hp",
+import { Ship } from "./battleship";
+
+test("2 length ship with full hp", () =>
   expect(Ship()).toEqual({
     length: 2,
     sunk: false,
     hit: 0,
-  })
-);
+  }));
