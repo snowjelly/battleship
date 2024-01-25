6f5887d9f5fec66034d4e6aa1096aaf6cda556e8
snowjelly <broadwellconnor@gmail.com>
2024-01-25 12:34:34 -0600

    find missed attacks test

--- battleship.test.js
+++ battleship.test.js
@@ -37,3 +37,11 @@ test("attacked ship at [9,0]", () => {
   board.placeShip(ship, [9, 0]);
   expect(board.receiveAttack([9, 0])).toMatchObject({ numTimesHit: 1 });
 });
+
+test("found all missed attacks", () => {
+  const board = Gameboard();
+  const ship = Ship(6);
+  board.placeShip(ship, [9, 0]);
+  board.receiveAttack([0, 0]);
+  expect(board.missedAttacks()).toContainEqual({ pos: [0, 0] });
+});
