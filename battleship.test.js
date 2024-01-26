94238270e730f47a386b6a0d5215bfc649ca82c4
snowjelly <broadwellconnor@gmail.com>
2024-01-26 10:22:09 -0600

    place ships randomly fail

--- battleship.test.js
+++ battleship.test.js
@@ -146,3 +146,9 @@ test("board generates list of usuable ships", () => {
     })
   );
 });
+
+test("place a ship in a random coordinate", () => {
+  const board = Gameboard();
+  board.placeShipsRandomly();
+  expect(board.shipsSunk().sunk).toBe(false);
+});
