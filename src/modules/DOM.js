93de2dedb26efda6bf630c9d8606f94afe5821d6
snowjelly <broadwellconnor@gmail.com>
2024-01-31 09:58:55 -0600

    renderNameSelection

--- src/modules/DOM.js
+++ src/modules/DOM.js
@@ -2,14 +2,6 @@ function removeWelcomeScreen() {
   document.querySelector(".welcome").remove();
 }
 
-function fadeOutWelcome() {
-  const welcome = document.querySelector(".welcome");
-  welcome.classList.add("fade-out");
-  welcome.addEventListener("animationend", () => {
-    removeWelcomeScreen();
-  });
-}
-
 function waitForAnimationEnd(animationClassName, querySelector, cb) {
   const element = document.querySelector(querySelector);
   element.classList.add(animationClassName);
@@ -18,16 +10,25 @@ function waitForAnimationEnd(animationClassName, querySelector, cb) {
   });
 }
 
-function renderNameSelection() {
+function renderNameSelection(ai) {
+  let player1Name;
+  let player2Name;
+  if (ai) {
+    player1Name = `Enter your name:`;
+    player2Name = `Enter the name for your A.I challenger:`;
+  } else {
+    player1Name = `Player 1: Enter your name:`;
+    player2Name = `Player 2: Enter your name:`;
+  }
   const html = `
     <div class="name-selection fade-in">
       <form>
-        <div class"form-name-el">
-          <label for="player1"> Player 1: Enter your name: </label>
+        <div class="form-name-el">
+          <label for="player1"> ${player1Name} </label>
           <input type="text" name="player1" id="player1" required/>
         </div>
-        <div class"form-name-el">
-          <label for="player2"> Player 2: Enter your name: </label>
+        <div class="form-name-el">
+          <label for="player2"> ${player2Name} </label>
           <input type="text" name="player2" id="player2" required/>
         </div>
       </form>
@@ -42,12 +43,14 @@ function addOpponentSelectEventListeners() {
 
   aiBtn.addEventListener("click", (e) => {
     waitForAnimationEnd("fade-out", ".welcome", removeWelcomeScreen);
-    waitForAnimationEnd("fade-out", ".welcome", renderNameSelection);
+    waitForAnimationEnd("fade-out", ".welcome", () => {
+      renderNameSelection(true);
+    });
   });
 
   humanBtn.addEventListener("click", (e) => {
-    fadeOutWelcome();
-    renderNameSelection();
+    waitForAnimationEnd("fade-out", ".welcome", removeWelcomeScreen);
+    waitForAnimationEnd("fade-out", ".welcome", renderNameSelection);
   });
 }
 
